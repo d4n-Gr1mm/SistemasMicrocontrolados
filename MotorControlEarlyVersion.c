ba00dca6452aac512997d03a17c2890a53515d1c
d4n-Gr1mm <danielgacuna@gmail.com>
2016-12-12 21:15:12 -0200

    Add another early version

--- MotorControlEarlyVersion.c
+++ MotorControlEarlyVersion.c
@@ -1,78 +1,32 @@
-unsigned short kp, cnt, oldstate = 0;
-char txt[6];
-// Keypad module connections
-char keypadPort at PORTD;
-// End Keypad module connections
-// LCD module connections
-sbit LCD_RS at RB2_bit;
-sbit LCD_EN at RB3_bit;
-sbit LCD_D4 at RB4_bit;
-sbit LCD_D5 at RB5_bit;
-sbit LCD_D6 at RB6_bit;
-sbit LCD_D7 at RB7_bit;
+unsigned short valor = 0;
 
-sbit LCD_RS_Direction at TRISB2_bit;
-sbit LCD_EN_Direction at TRISB3_bit;
-sbit LCD_D4_Direction at TRISB4_bit;
-sbit LCD_D5_Direction at TRISB5_bit;
-sbit LCD_D6_Direction at TRISB6_bit;
-sbit LCD_D7_Direction at TRISB7_bit;
-// End LCD module connections
-void main() 
+void main() {
+
+TRISB.RB0 = 1;  //BOTÃO DE INCREMENTO do PWM
+TRISB.RB1 = 1;  //BOTÃO DE INCREMENTO do PWM
+
+TRISC.RC3 = 0;  //SAIDA DO PWM
+PORTB.RC3 = 0;
+
+PWM1_Init(5000);
+PWM1_Set_Duty(0);
+PWM1_Start();
+TMR2=0;
+
+while(1)
 {
-  cnt = 0; // Reset counter
-  Keypad_Init(); // Initialize Keypad
-  Lcd_Init(); // Initialize Lcd
-  Lcd_Cmd(_LCD_CLEAR); // Clear display
-  Lcd_Cmd(_LCD_CURSOR_OFF); // Cursor off
-  Lcd_Out(1, 1, "1");
-  Lcd_Out(1, 1, "Key :"); // Write message text on Lcd
-  Lcd_Out(2, 1, "Times:");
-  do 
-  {
-    kp = 0; // Reset key code variable
-    // Wait for key to be pressed and released
-    do
-      //kp = Keypad_Key_Press(); // Store key code in kp variable
-      kp = Keypad_Key_Click(); // Store key code in kp variable
-    while (!kp);
-    // Prepare value for output, transform key to it's ASCII value
-    switch (kp) 
-    {
-      case 1: kp = 49; break; // 1
-      case 2: kp = 50; break; // 2
-      case 3: kp = 51; break; // 3
-      //case 4: kp = 65; break; // A commented since 4th column is absent for 4*3 keypad
-      case 5: kp = 52; break; // 4
-      case 6: kp = 53; break; // 5
-      case 7: kp = 54; break; // 6
-      //case 8: kp = 66; break; // B commented since 4th column is absent for 4*3 keypad
-      case 9: kp = 55; break; // 7
-      case 10: kp = 56; break; // 8
-      case 11: kp = 57; break; // 9
-      //case 12: kp = 67; break; // C commented since 4th column is absent for 4*3 keypad
-      case 13: kp = 42; break; // *
-      case 14: kp = 48; break; // 0
-      case 15: kp = 35; break; // #
-      //case 16: kp = 68; break; // D
-    }
-    if (kp != oldstate) 
-    { // Pressed key differs from previous
-      cnt = 1;
-      oldstate = kp;
-    }
-    else 
-    { // Pressed key is same as previous
-      cnt++;
-    }
-    Lcd_Chr(1, 10, kp); // Print key ASCII value on Lcd
-    if (cnt == 255) 
-    { // If counter varialble overflow
-      cnt = 0;
-      Lcd_Out(2, 10, " ");
-    }
+        if (PORTB.RB0==1 && valor < 255){
+        delay_ms(200);
+        valor = valor + 5;
+        PWM1_Set_Duty(valor);
+        TMR2=0;
+        }
 
-    WordToStr(cnt, txt); // Transform counter value to string
-    Lcd_Out(2, 10, txt); // Display counter value on Lcd
- } while (1);
+        if (PORTB.RB1==1 && valor >= 5){
+        delay_ms(200);
+        valor = valor - 5;
+        PWM1_Set_Duty(valor);
+        TMR2=0;
+        }
+}
 }
